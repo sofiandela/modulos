3fcd480fe7849c0b3518cb065a97c0e6b36b1c0a
Federico Perez <federico.perez@globallogic.com>
2019-01-24 17:24:01 -0300

    Updated .babelrc, src -> index html and js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
-console.log("asasas");
+let arr = ['a','b','c','d'];
+
+console.log("este es el index 4:", ...arr);
+
+let arrowFunction = () => {
+    console.log("llamada a arrowFunction");
+}
+arrowFunction();
