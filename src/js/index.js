3752ab86151ce7edb4b33c76124e83292b6dda9c
Federico Perez <federico.perez@globallogic.com>
2019-01-24 17:36:05 -0300

    Test export and import from test.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 let arr = ['a','b','c','d'];
 
-console.log("este es el index 4:", ...arr);
+// console.log("este es el index 4:", ...arr);
 
 let arrowFunction = () => {
-    console.log("llamada a arrowFunction");
+    // console.log("llamada a arrowFunction");
 }
 arrowFunction();
+
+import num from './test';
+
+console.log("Num desde index: ",num);
---
+++ src/js/test.js
@@ -0,0 +1,3 @@
+console.log("Desde test.js");
+
+export default 15;
